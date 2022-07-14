799a4b3723bd6908442ff52c82e034a174d2dd0e
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-07-14 08:36:39 -0700

    created the roll method

--- DieRoller/Program.cs
+++ DieRoller/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using DieRoller;
+
+Die myDie = new Die(); // when a die is created, a random faceValue should be set
+byte currentValue = myDie.FaceValue;
+
+myDie.Roll();
+myDie.Roll();
+myDie.Roll();
+myDie.Roll();
+
+Console.ReadKey();
