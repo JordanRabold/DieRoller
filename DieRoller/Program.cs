3c99172046a0e65a8d83458458d8eb27cfa32225
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-07-14 09:11:53 -0700

    Added some tests

--- DieRoller/Program.cs
+++ DieRoller/Program.cs
@@ -1,11 +1,30 @@
 ﻿using DieRoller;
 
 Die myDie = new Die(); // when a die is created, a random faceValue should be set
-byte currentValue = myDie.FaceValue;
 
 myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
 myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
 myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
 myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+myDie.IsHeld = true;
+Console.WriteLine("Holding the die");
+
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+myDie.IsHeld = true;
+Console.WriteLine("Holding the die");
+
 
 Console.ReadKey();
